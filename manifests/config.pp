85d3e804f32a081eda3e7c06f2b6b23afb0e4d62
Chris Smith <casmit14@ncsu.edu>
2017-06-14 17:53:18 -0400

    Don't check config until fact is in place

--- manifests/config.pp
+++ manifests/config.pp
@@ -66,132 +66,133 @@ class wds::config (
   Boolean $transport_multicast_session_policy_fallback = $::wds::params::transport_multicast_session_policy_fallback,
   Boolean $transport_force_native = $::wds::params::transport_force_native,
 ) inherits wds {
-
-  if $answer_clients != 'All' and $answer_clients != 'Known' and $answer_clients != 'None' {
-    fail("::wds::config::answer_clients (${answer_clients}) must be one of 'All', 'Known', or 'None'")
-  }
-
-  if $response_delay < 0 {
-    fail("::wds::config::response_delay (${response_delay}) must be greater than or equal to 0")
-  }
-
-  if $default_x86_x64_image_type != 'x86' and $default_x86_x64_image_type != 'x64' and $default_x86_x64_image_type != 'Both' {
-    fail("::wds::config::default_x86_x64_image_type (${default_x86_x64_image_type}) must be one of 'x86', 'x64', or 'Both'")
-  }
-
-  if $rpc_port <= 0 {
-    fail("::wds::config::rpc_port (${rpc_port}) must be greater than 0")
-  }
-
-  if $pxe_prompt_policy_known != 'OptIn' and $pxe_prompt_policy_known != 'NoPrompt' and $pxe_prompt_policy_known != 'OptOut' {
-    fail("::wds::config::default_x86_x64_image_type (${pxe_prompt_policy_known}) must be one of 'OptIn', 'NoPrompt', or 'OptOut'")
-  }
-
-  if $pxe_prompt_policy_new != 'OptIn' and $pxe_prompt_policy_new != 'NoPrompt' and $pxe_prompt_policy_new != 'OptOut' {
-    fail("::wds::config::default_x86_x64_image_type (${pxe_prompt_policy_new}) must be one of 'OptIn', 'NoPrompt', or 'OptOut'")
-  }
-
-  if $new_machine_type != 'ServerDomain' and $new_machine_type != 'UserDomain' and $new_machine_type != 'UserOU' and $new_machine_type != 'Custom' {
-    fail("::wds::config::default_x86_x64_image_type (${new_machine_type}) must be one of 'ServerDomain', 'UserDomain', 'UserOU', or 'Custom'")
-  }
-
-  if $domain_search_order != 'GCOnly' and $domain_search_order != 'DCFirst' {
-    fail("::wds::config::domain_search_order (${domain_search_order}) must be 'GCOnly' or 'DCFirst'")
-  }
-
-  if $wds_client_logging_level != 'None' and $wds_client_logging_level != 'Errors' and $wds_client_logging_level != 'Warnings' and $wds_client_logging_level != 'Info' {
-    fail("::wds::config::wds_client_logging_level (${wds_client_logging_level}) must be one of 'None', 'Errors', 'Warnings', or 'Info'")
-  }
-
-  if $auto_add_policy != 'AdminApproval' and $auto_add_policy != 'Disabled' {
-    fail("::wds::config::auto_add_policy (${auto_add_policy}) must be 'AdminApproval' or 'Disabled'")
-  }
-
-  if $auto_add_policy_poll_interval < 0 {
-    fail("::wds::config::auto_add_policy_poll_interval (${auto_add_policy_poll_interval}) must be greater than or equal to 0")
-  }
-
-  if $auto_add_policy_max_retry_count < 0 {
-    fail("::wds::config::auto_add_policy_max_retry_count (${auto_add_policy_max_retry_count}) must be greater than or equal to 0")
-  }
-
-  if $auto_add_policy_retention_period_approved < 0 {
-    fail("::wds::config::auto_add_policy_retention_period_approved (${auto_add_policy_retention_period_approved}) must be greater than or equal to 0")
-  }
-
-  if $auto_add_policy_retention_period_others < 0 {
-    fail("::wds::config::auto_add_policy_retention_period_others (${auto_add_policy_retention_period_others}) must be greater than or equal to 0")
-  }
-
-  if $bind_policy != 'Include' and $bind_policy != 'Exclude' {
-    fail("::wds::config::bind_policy (${bind_policy}) must be 'Include' or 'Exclude'")
-  }
-
-  if $refresh_period < 0 {
-    fail("::wds::config::refresh_period (${refresh_period}) must be greater than or equal to 0")
-  }
-
-  if $bcd_refresh_policy_period < 0 {
-    fail("::wds::config::bcd_refresh_policy_period (${bcd_refresh_policy_period}) must be greater than or equal to 0")
-  }
-
-  if $transport_obtain_ipv4_from != 'Dhcp' and $transport_obtain_ipv4_from != 'Range' {
-    fail("::wds::config::transport_obtain_ipv4_from (${transport_obtain_ipv4_from}) must be 'Dhcp' or 'Range'")
-  }
-
-  if $transport_obtain_ipv6_from != 'Dhcp' and $transport_obtain_ipv6_from != 'Range' {
-    fail("::wds::config::transport_obtain_ipv6_from (${transport_obtain_ipv6_from}) must be 'Dhcp' or 'Range'")
-  }
-
-  if $transport_start_port < 0 {
-    fail("::wds::config::transport_start_port (${transport_start_port}) must be greater than or equal to 0")
-  }
-
-  if $transport_end_port < 0 {
-    fail("::wds::config::transport_end_port (${transport_end_port}) must be greater than or equal to 0")
-  }
-
-  if $transport_profile != '10Mbps' and $transport_profile != '100Mbps' and $transport_profile != '1Gbps' and $transport_profile != 'Custom' {
-    fail("::wds::config::transport_profile (${transport_profile}) must be one of '10Mbps', '100Mbps', '1Gbps', or 'Custom'")
-  }
-
-  if $transport_multicast_session_policy != 'None' and $transport_multicast_session_policy != 'AutoDisconnect' and $transport_multicast_session_policy != 'Multistream' {
-    fail("::wds::config::transport_multicast_session_policy (${transport_multicast_session_policy}) must be one of 'None', 'AutoDisconnect', or 'Multistream'")
-  }
-
-  if $transport_multicast_session_policy_threshold < 0 {
-    fail("::wds::config::transport_multicast_session_policy_threshold (${transport_multicast_session_policy_threshold}) must be greater than or equal to 0")
-  }
-
-  if $transport_multicast_session_policy_stream_count != 2 and $transport_multicast_session_policy_stream_count != 3 {
-    fail("::wds::config::transport_multicast_session_policy_stream_count (${transport_multicast_session_policy_stream_count}) must be 2 or 3")
-  }
-
-  if $::wds::config::remote_install_location != $::wds_conf['installation_state']['remoteinstall_location'] {
-    exec { 'Uninitialize WDS Server - Remote Install Path Change':
-      command => "C:\\Windows\\System32\\wdsutil.exe /Uninitialize-Server",
-      before  => Exec['Initialize WDS Server'],
+  if $::wds_conf {
+    if $answer_clients != 'All' and $answer_clients != 'Known' and $answer_clients != 'None' {
+      fail("::wds::config::answer_clients (${answer_clients}) must be one of 'All', 'Known', or 'None'")
     }
-  }
 
-  exec { 'Initialize WDS Server':
-    command => "C:\\Windows\\System32\\wdsutil.exe /Initialize-Server /reminst:\"${::wds::config::remote_install_location}\"",
-    unless  => 'C:\\Windows\\System32\\wdsutil.exe /Get-Server /Show:All',
-  }
+    if $response_delay < 0 {
+      fail("::wds::config::response_delay (${response_delay}) must be greater than or equal to 0")
+    }
 
-  class { '::wds::config::apply_settings':
-    require => Exec['Initialize WDS Server'],
-  }
+    if $default_x86_x64_image_type != 'x86' and $default_x86_x64_image_type != 'x64' and $default_x86_x64_image_type != 'Both' {
+      fail("::wds::config::default_x86_x64_image_type (${default_x86_x64_image_type}) must be one of 'x86', 'x64', or 'Both'")
+    }
 
-  if $::wds::config::enable_service {
-    exec { 'Enable WDS Services':
-      command => 'C:\Windows\System32\wdsutil.exe /Enable-Server',
+    if $rpc_port <= 0 {
+      fail("::wds::config::rpc_port (${rpc_port}) must be greater than 0")
+    }
+
+    if $pxe_prompt_policy_known != 'OptIn' and $pxe_prompt_policy_known != 'NoPrompt' and $pxe_prompt_policy_known != 'OptOut' {
+      fail("::wds::config::default_x86_x64_image_type (${pxe_prompt_policy_known}) must be one of 'OptIn', 'NoPrompt', or 'OptOut'")
+    }
+
+    if $pxe_prompt_policy_new != 'OptIn' and $pxe_prompt_policy_new != 'NoPrompt' and $pxe_prompt_policy_new != 'OptOut' {
+      fail("::wds::config::default_x86_x64_image_type (${pxe_prompt_policy_new}) must be one of 'OptIn', 'NoPrompt', or 'OptOut'")
+    }
+
+    if $new_machine_type != 'ServerDomain' and $new_machine_type != 'UserDomain' and $new_machine_type != 'UserOU' and $new_machine_type != 'Custom' {
+      fail("::wds::config::default_x86_x64_image_type (${new_machine_type}) must be one of 'ServerDomain', 'UserDomain', 'UserOU', or 'Custom'")
+    }
+
+    if $domain_search_order != 'GCOnly' and $domain_search_order != 'DCFirst' {
+      fail("::wds::config::domain_search_order (${domain_search_order}) must be 'GCOnly' or 'DCFirst'")
+    }
+
+    if $wds_client_logging_level != 'None' and $wds_client_logging_level != 'Errors' and $wds_client_logging_level != 'Warnings' and $wds_client_logging_level != 'Info' {
+      fail("::wds::config::wds_client_logging_level (${wds_client_logging_level}) must be one of 'None', 'Errors', 'Warnings', or 'Info'")
+    }
+
+    if $auto_add_policy != 'AdminApproval' and $auto_add_policy != 'Disabled' {
+      fail("::wds::config::auto_add_policy (${auto_add_policy}) must be 'AdminApproval' or 'Disabled'")
+    }
+
+    if $auto_add_policy_poll_interval < 0 {
+      fail("::wds::config::auto_add_policy_poll_interval (${auto_add_policy_poll_interval}) must be greater than or equal to 0")
+    }
+
+    if $auto_add_policy_max_retry_count < 0 {
+      fail("::wds::config::auto_add_policy_max_retry_count (${auto_add_policy_max_retry_count}) must be greater than or equal to 0")
+    }
+
+    if $auto_add_policy_retention_period_approved < 0 {
+      fail("::wds::config::auto_add_policy_retention_period_approved (${auto_add_policy_retention_period_approved}) must be greater than or equal to 0")
+    }
+
+    if $auto_add_policy_retention_period_others < 0 {
+      fail("::wds::config::auto_add_policy_retention_period_others (${auto_add_policy_retention_period_others}) must be greater than or equal to 0")
+    }
+
+    if $bind_policy != 'Include' and $bind_policy != 'Exclude' {
+      fail("::wds::config::bind_policy (${bind_policy}) must be 'Include' or 'Exclude'")
+    }
+
+    if $refresh_period < 0 {
+      fail("::wds::config::refresh_period (${refresh_period}) must be greater than or equal to 0")
+    }
+
+    if $bcd_refresh_policy_period < 0 {
+      fail("::wds::config::bcd_refresh_policy_period (${bcd_refresh_policy_period}) must be greater than or equal to 0")
+    }
+
+    if $transport_obtain_ipv4_from != 'Dhcp' and $transport_obtain_ipv4_from != 'Range' {
+      fail("::wds::config::transport_obtain_ipv4_from (${transport_obtain_ipv4_from}) must be 'Dhcp' or 'Range'")
+    }
+
+    if $transport_obtain_ipv6_from != 'Dhcp' and $transport_obtain_ipv6_from != 'Range' {
+      fail("::wds::config::transport_obtain_ipv6_from (${transport_obtain_ipv6_from}) must be 'Dhcp' or 'Range'")
+    }
+
+    if $transport_start_port < 0 {
+      fail("::wds::config::transport_start_port (${transport_start_port}) must be greater than or equal to 0")
+    }
+
+    if $transport_end_port < 0 {
+      fail("::wds::config::transport_end_port (${transport_end_port}) must be greater than or equal to 0")
+    }
+
+    if $transport_profile != '10Mbps' and $transport_profile != '100Mbps' and $transport_profile != '1Gbps' and $transport_profile != 'Custom' {
+      fail("::wds::config::transport_profile (${transport_profile}) must be one of '10Mbps', '100Mbps', '1Gbps', or 'Custom'")
+    }
+
+    if $transport_multicast_session_policy != 'None' and $transport_multicast_session_policy != 'AutoDisconnect' and $transport_multicast_session_policy != 'Multistream' {
+      fail("::wds::config::transport_multicast_session_policy (${transport_multicast_session_policy}) must be one of 'None', 'AutoDisconnect', or 'Multistream'")
+    }
+
+    if $transport_multicast_session_policy_threshold < 0 {
+      fail("::wds::config::transport_multicast_session_policy_threshold (${transport_multicast_session_policy_threshold}) must be greater than or equal to 0")
+    }
+
+    if $transport_multicast_session_policy_stream_count != 2 and $transport_multicast_session_policy_stream_count != 3 {
+      fail("::wds::config::transport_multicast_session_policy_stream_count (${transport_multicast_session_policy_stream_count}) must be 2 or 3")
+    }
+
+    if $::wds::config::remote_install_location != $::wds_conf['installation_state']['remoteinstall_location'] {
+      exec { 'Uninitialize WDS Server - Remote Install Path Change':
+        command => "C:\\Windows\\System32\\wdsutil.exe /Uninitialize-Server",
+        before  => Exec['Initialize WDS Server'],
+      }
+    }
+
+    exec { 'Initialize WDS Server':
+      command => "C:\\Windows\\System32\\wdsutil.exe /Initialize-Server /reminst:\"${::wds::config::remote_install_location}\"",
+      unless  => 'C:\\Windows\\System32\\wdsutil.exe /Get-Server /Show:All',
+    }
+
+    class { '::wds::config::apply_settings':
       require => Exec['Initialize WDS Server'],
     }
-  } else {
-    exec { 'Disable WDS Services':
-      command => 'C:\Windows\System32\wdsutil.exe /Disable-Server',
-      require => Exec['Initialize WDS Server'],
+
+    if $::wds::config::enable_service {
+      exec { 'Enable WDS Services':
+        command => 'C:\Windows\System32\wdsutil.exe /Enable-Server',
+        require => Exec['Initialize WDS Server'],
+      }
+    } else {
+      exec { 'Disable WDS Services':
+        command => 'C:\Windows\System32\wdsutil.exe /Disable-Server',
+        require => Exec['Initialize WDS Server'],
+      }
     }
   }
 }
