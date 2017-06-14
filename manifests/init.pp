1b4c22428dff7a37afe8cdeb83c953423b28038d
Chris Smith <casmit14@ncsu.edu>
2017-06-14 17:10:26 -0400

    missing commas

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,12 @@
 # Class: wds
 # Initialize WDS
 class wds(
-  $install_feature     = $wds::params::install_feature,
-  $ensure_service      = $wds::params::ensure_service
-  $enable_service      = $wds::params::enable_service
-  $feature_name        = $wds::params::feature_name
-  $remote_install_path = $wds::params::remote_install_path
-) inherits $wds::params {
+  $install_feature     = $::wds::params::install_feature,
+  $ensure_service      = $::wds::params::ensure_service,
+  $enable_service      = $::wds::params::enable_service,
+  $feature_name        = $::wds::params::feature_name,
+  $remote_install_path = $::wds::params::remote_install_path,
+) inherits wds::params {
   if $::osfamily == 'Windows' {
     validate_boolean($install_feature)
     validate_string($ensure_service)
