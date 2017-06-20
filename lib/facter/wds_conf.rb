baf7e0ec7dde6a282cc05dcad04b9320c6fc2663
Chris Smith <casmit14@ncsu.edu>
2017-06-20 17:01:07 -0400

    Let's see if we can get a structured fact for wds

---
+++ lib/facter/wds_conf.rb
@@ -0,0 +1,73 @@
+Facter.add(:wds_conf) do
+  setcode do
+    $settingsRaw = Facter::Util::Resolution.exec('wdsutil /Get-Server /Show:Config').split('\r\n')
+
+    def processLines(pl)
+      $section = {}
+      $level = 0
+      $previousLevel = pl
+
+      while $ln <= $settingsRaw.length do
+        $line = $settingsRaw[ln]
+
+        if $line
+          $level = $line.length - $line.lstrip.length
+
+          if $level < $previousLevel
+            $ln -= 1
+            break
+          end
+
+          if $line.include? ":" and ($settingsRaw[ln+1].length - $settingsRaw[ln+1].lstrip.length) > $level
+            $new_section = $line.strip.downcase.sub(":","").sub(" ","_")
+            $ln += 1
+
+            if $new_section == "banned_guids_list" and !($settingsRaw[ln].index(" ") == 0)
+                $section[$new_section] = {}
+            else
+              $section[$new_section] = processLines($level)
+            end
+
+            if $new_section == "wds_unattend_files" and $section[$new_section].length = 0
+              $section[$new_section] = { 'x86'=>'', 'x64'=>'', 'ia64'=>'' }
+            end
+          elsif $line.index(" ") == 0
+            if $line.include? ":"
+              $setting = $line.strip.sub(":.*","")
+              $value = $line.sub($setting + ":","").strip
+
+              if $value.include? " second"
+                $value = $value.sub(" second.*","")
+              elsif $value.index? " minute"
+                $value = $value.sub(" minute.*","")
+              elsif $value.index? " hour"
+                $value = $value.sub(" hour.*","")
+              elsif $value.index? " day"
+                $value = $value.sub(" day.*","")
+              elsif $value.index? " time"
+                $value = $value.sub(" time.*","")
+              end
+
+              $setting = $setting.downcase.sub(" ","_")
+              $section[$setting] = $value
+            elsif $line.index? " - "
+              $setting = $line.strip.sub("[ ]+-.*","")
+              $value = $line.sub($setting + "[ ]*-","").strip
+              $setting = $setting.downcase.sub(" ","_")
+              $section[$setting] = $value
+            end
+          end
+
+          $previousLevel = $level
+        end
+
+        $ln += 1
+      end
+
+      $section
+    end
+
+    $ln = 0
+    processLines(0)
+  end
+end
--- manifests/install.pp
+++ manifests/install.pp
@@ -5,15 +5,5 @@ class wds::install inherits wds {
       dsc_ensure => 'present',
       dsc_name   => $::wds::feature_name,
     }
-
-    file { ['C:\ProgramData\PuppetLabs\facter', 'C:\ProgramData\PuppetLabs\facter\facts.d']:
-      ensure => directory,
-      owner  => 'SYSTEM',
-    }->
-    file { 'C:\ProgramData\PuppetLabs\facter\facts.d\wds_conf_fact.ps1':
-      ensure => file,
-      owner  => 'SYSTEM',
-      source => 'puppet:///modules/wds/wds_conf_fact.ps1',
-    }
   }
 }
