1b65bbc1f96a8e5f15a8bd7b8dec9971e9d98cda
Chris Smith <casmit14@ncsu.edu>
2017-06-23 15:35:58 -0400

    remove unneeded param and puppet lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,14 +5,13 @@ class wds(
   String $ensure_service      = $::wds::params::ensure_service,
   Boolean $enable_service      = $::wds::params::enable_service,
   String $feature_name        = $::wds::params::feature_name,
-  String $remote_install_path = $::wds::params::remote_install_path,
 ) inherits wds::params {
   if $::osfamily == 'Windows' {
-    anchor{'wds::begin':} ->
-    class{'::wds::install':} ->
-    class{'::wds::config':} ~>
-    class{'::wds::service':} ->
-    anchor{'wds::end':}
+    anchor{'wds::begin':}
+    -> class{'::wds::install':}
+    -> class{'::wds::config':}
+    ~> class{'::wds::service':}
+    -> anchor{'wds::end':}
   } else {
     fail { "This operating system family (${::osfamily}) is not supported.": }
   }
