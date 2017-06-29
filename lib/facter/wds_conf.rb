41db206e858cff417a853bc4aa6b55cd40884070
Chris Smith <casmit14@ncsu.edu>
2017-06-29 13:22:50 -0400

    whitespace and check for empty raw settings

--- lib/facter/wds_conf.rb
+++ lib/facter/wds_conf.rb
@@ -1,84 +1,87 @@
 Facter.add(:wds_conf) do
   confine :osfamily => "Windows"
-  
+
   setcode do
     $settingsRaw = Facter::Util::Resolution.exec('wdsutil /Get-Server /Show:Config')
-    $settingsRaw.encode!('UTF-8', 'UTF-16', :invalid => :replace, :undef => :replace, :replace => '', :universal_newline => true)
-    $settings = $settingsRaw.split("\n")
-    
-    def processLines(pl)
-      section = {}
-      level = 0
-      previousLevel = pl
 
-      while $ln < $settings.length do
-        line = $settings[$ln]
-        
-        if !line.to_s.strip.empty?
-          level = line.length - line.lstrip.length
+    if $settingsRaw
+      $settingsRaw.encode!('UTF-8', 'UTF-16', :invalid => :replace, :undef => :replace, :replace => '', :universal_newline => true)
+      $settings = $settingsRaw.split("\n")
 
-          if level < previousLevel
-            $ln -= 1
-            break
-          end
+      def processLines(pl)
+	section = {}
+	level = 0
+	previousLevel = pl
 
-          if line.include? ':' and ($ln+1) < $settings.length and ($settings[$ln+1].length - $settings[$ln+1].lstrip.length) > level
-            new_section = line.strip.downcase.gsub(':','').gsub(' ','_')
-            $ln += 1
+	while $ln < $settings.length do
+	  line = $settings[$ln]
 
-            if new_section == 'banned_guids_list' and !($settings[$ln].index(' ') == 0)
-                section[new_section] = {}
-            else
-              section[new_section] = processLines(level)
-            end
+	  if !line.to_s.strip.empty?
+	    level = line.length - line.lstrip.length
 
-            if new_section == 'wds_unattend_files' and section[new_section].length == 0
-              section[new_section] = { 'x86'=>'', 'x64'=>'', 'ia64'=>'' }
-            end
-          elsif line.index(' ') == 0
-            if line.include? ':'
-              setting = line.strip.sub(/:.*/,'')
-              value = line.sub(setting + ':','').strip
+	    if level < previousLevel
+	      $ln -= 1
+	      break
+	    end
 
-              if value.include? ' second'
-                value = value.sub(/ second.*/,'')
-              elsif value.include? ' minute'
-                value = value.sub(/ minute.*/,'')
-              elsif value.include? ' hour'
-                value = value.sub(/ hour.*/,'')
-              elsif value.include? ' day'
-                value = value.sub(/ day.*/,'')
-              elsif value.include? ' time'
-                value = value.sub(/ time.*/,'')
-              elsif value.include? ' KBps'
-                value = value.sub(/ KBps.*/,'')
-              end
+	    if line.include? ':' and ($ln+1) < $settings.length and ($settings[$ln+1].length - $settings[$ln+1].lstrip.length) > level
+	      new_section = line.strip.downcase.gsub(':','').gsub(' ','_')
+	      $ln += 1
 
-              setting = setting.downcase.gsub(' ','_')
+	      if new_section == 'banned_guids_list' and !($settings[$ln].index(' ') == 0)
+		section[new_section] = {}
+	      else
+		section[new_section] = processLines(level)
+	      end
 
-              if value =~ /\A[-+]?[0-9]+\z/
-                      section[setting] = value.to_i
-              else
-                section[setting] = value
-              end
-            elsif line.include? ' - '
-              setting = line.strip.sub(/[ ]+-.*/,'')
-              value = line.sub(/^.*[ ]+- /,'').strip
-              setting = setting.downcase.gsub(' ','_')
-              section[setting] = value
-            end
-          end
+	      if new_section == 'wds_unattend_files' and section[new_section].length == 0
+		section[new_section] = { 'x86'=>'', 'x64'=>'', 'ia64'=>'' }
+	      end
+	    elsif line.index(' ') == 0
+	      if line.include? ':'
+		setting = line.strip.sub(/:.*/,'')
+		value = line.sub(setting + ':','').strip
 
-          previousLevel = level
-        end
+		if value.include? ' second'
+		  value = value.sub(/ second.*/,'')
+		elsif value.include? ' minute'
+		  value = value.sub(/ minute.*/,'')
+		elsif value.include? ' hour'
+		  value = value.sub(/ hour.*/,'')
+		elsif value.include? ' day'
+		  value = value.sub(/ day.*/,'')
+		elsif value.include? ' time'
+		  value = value.sub(/ time.*/,'')
+		elsif value.include? ' KBps'
+		  value = value.sub(/ KBps.*/,'')
+		end
 
-        $ln += 1
+		setting = setting.downcase.gsub(' ','_')
+
+		if value =~ /\A[-+]?[0-9]+\z/
+		  section[setting] = value.to_i
+		else
+		  section[setting] = value
+		end
+	      elsif line.include? ' - '
+		setting = line.strip.sub(/[ ]+-.*/,'')
+		value = line.sub(/^.*[ ]+- /,'').strip
+		setting = setting.downcase.gsub(' ','_')
+		section[setting] = value
+	      end
+	    end
+
+	    previousLevel = level
+	  end
+
+	  $ln += 1
+	end
+
+	section
       end
 
-      section
+      $ln = 0
+      processLines(0)
     end
-
-    $ln = 0
-    processLines(0)
   end
 end
