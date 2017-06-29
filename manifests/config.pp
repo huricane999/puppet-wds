1d9c0c89e40f8c7cf49090fe3788065b1fff4282
Chris Smith <casmit14@ncsu.edu>
2017-06-29 14:54:33 -0400

    if no installation state don't uninitialize

--- manifests/config.pp
+++ manifests/config.pp
@@ -185,8 +185,7 @@ class wds::config (
   }
 
   if $::wds_conf {
-    if !has_key($::wds_conf, 'installation_state') or !has_key($::wds_conf['installation_state'], 'remoteinstall_location') or $remote_install_path != $::wds_conf['installation_state']['remoteinstall_location'] {
-
+    if has_key($::wds_conf, 'installation_state') and has_key($::wds_conf['installation_state'], 'remoteinstall_location') and $remote_install_path != $::wds_conf['installation_state']['remoteinstall_location'] {
       exec { 'Uninitialize WDS Server - Remote Install Path Change':
         command => 'C:\\Windows\\System32\\wdsutil.exe /Uninitialize-Server',
         before  => Exec['Initialize WDS Server'],
